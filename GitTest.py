17552a7f9efe6e90cedcc2b21fa88000a05b6546
njahn88 <nja010@email.latech.edu>
2022-03-18 13:21:17 -0500

    Update GitTest.py
    Changed the size of the window, deleted print statement

--- GitTest.py
+++ GitTest.py
@@ -1,8 +1,9 @@
 import tkinter as tk
+from turtle import width
 
 window = tk.Tk()
 
-window.attributes('-fullscreen', True)
+window.geometry(f"{800}x{600}")
 window.title("Project")
 
 button1 = tk.Button(window, text="Test button")
@@ -11,6 +12,5 @@ button1.grid(row=0, column=0)
 button2 = tk.Button(window, text="Quit", command =window.destroy)
 button2.grid(row=0, column=1)
 
-print("Change")
 
 window.mainloop()
