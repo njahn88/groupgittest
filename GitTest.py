2c6670ca6fc94276c0eb5eae2b312ef1e2b6fd1b
njahn88 <nja010@email.latech.edu>
2022-03-18 11:04:25 -0500

    go back to cecilia's change

--- GitTest.py
+++ GitTest.py
@@ -9,7 +9,10 @@ button1 = tk.Button(window, text="Test button")
 button1.grid(row=0, column=0)
 
 print("Change")
+<<<<<<< HEAD
 print("change 2")
 print("nathans change")
+=======
+>>>>>>> parent of 5ab49c5 (change 2)
 
 window.mainloop()
